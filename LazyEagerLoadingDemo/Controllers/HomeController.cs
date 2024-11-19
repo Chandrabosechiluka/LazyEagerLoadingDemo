903be9da4b36012f1f14f0c36e813c1c42037300
CHANDRA <chandrabosechiluka30@gmail.com>
2024-11-19 21:59:11 +0530

    Add project files.

---
+++ LazyEagerLoadingDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using LazyEagerLoadingDemo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LazyEagerLoadingDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("lazy-loading")]
+        public async Task<IActionResult> GetLazyLoading([FromServices] MyContext context)
+        {
+            var authors = await context.Authors.ToListAsync(); 
+            var books = authors.SelectMany(a => a.Books); 
+            return Ok(authors);
+        }
+
+        [HttpGet("eager-loading")]
+        public async Task<IActionResult> GetEagerLoading(MyContext context)
+        {
+            var authorsWithBooks = await context.Authors
+                .Include(a => a.Books) 
+                .ToListAsync();
+            return Ok(authorsWithBooks);
+        }
+    }
+}
---
+++ LazyEagerLoadingDemo/Data/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using LazyEagerLoadingDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LazyEagerLoadingDemo.Data
+{
+    public class MyContext: DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+    }
+}
